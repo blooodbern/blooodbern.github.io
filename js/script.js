409887b76b3ad57e2beaf1f7d5c4166bca3a3b41
blooodbern <6906312@stud.nau.edu.ua>
2024-02-02 02:09:38 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ function gotoLink(link) {
 	window.open(link.value);
 }
 function gotoHome(link) {
-  location.href = link.value;
+	location.href = link.value;
 }
 let upButton = document.getElementById("upBtn");
-
-window.onscroll = function() {scrollFunction()};
 function topFunction() {
-  document.documentElement.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
